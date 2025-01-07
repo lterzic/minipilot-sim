5f304e11a089db19fa9645d02fbaff3fc95f6df8
Luka Terzic <lukaterzicbg@gmail.com>
2025-01-07 21:24:20 +0100

    added gyroscope

--- src/drivers/gyroscope_pb.cpp
+++ src/drivers/gyroscope_pb.cpp
@@ -1,6 +1,6 @@
-#include "accelerometer_pb.hpp"
+#include "gyroscope_pb.hpp"
 #include "protobuf/bridge.pb.h"
-#include "protobuf/read_acc.pb.h"
+#include "protobuf/read_gyro.pb.h"
 #include <mutex>
 #include <string>
 
@@ -8,36 +8,36 @@ static constexpr size_t RECV_BUFFER_SIZE = 256;
 
 namespace mpsim {
 
-bool accelerometer_pb::read_all_axes(float (&out_data)[3]) noexcept
+bool gyroscope_pb::read_all_axes(float (&out_data)[3]) noexcept
 {
     // Lock the mutex so that other drivers which communicate via this socket don't interrupt
     std::scoped_lock lock(m_socket_mutex);
 
-    // Send a protobuf message with a read acc request
+    // Send a protobuf message with a read gyro request
     mpsim::Request request;
     std::string out_bytes;
     
-    request.mutable_read_acc();
+    request.mutable_read_gyro();
     if (!request.SerializeToString(&out_bytes))
         return false;
     if (m_socket.write(out_bytes.c_str(), out_bytes.size()) != out_bytes.size())
         return false;
 
-    // Wait for a protobuf message and assert it is of type read acc response
+    // Wait for a protobuf message and assert it is of type read gyro response
     char buffer[RECV_BUFFER_SIZE];
     mpsim::Response response;
     
     ssize_t recv_size = m_socket.read(buffer, RECV_BUFFER_SIZE);
     if (recv_size <= 0 || !response.ParseFromArray(buffer, recv_size))
         return false;
-    if (response.response_type_case() != mpsim::Response::ResponseTypeCase::kReadAcc)
+    if (response.response_type_case() != mpsim::Response::ResponseTypeCase::kReadGyro)
         return false;
 
     // Put the data in the output array in the default order (x, y, z)
-    // This can be rearranged in the mp::task_accelerometer if needed
-    out_data[0] = response.read_acc().acc().x();
-    out_data[1] = response.read_acc().acc().y();
-    out_data[2] = response.read_acc().acc().z();
+    // This can be rearranged in the mp::task_gyroscope if needed
+    out_data[0] = response.read_gyro().ang_vel().x();
+    out_data[1] = response.read_gyro().ang_vel().y();
+    out_data[2] = response.read_gyro().ang_vel().z();
     return true;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
-#include "mp/main.hpp"
-#include "drivers/accelerometer.hpp"
+#include "drivers/accelerometer_pb.hpp"
+#include "drivers/gyroscope_pb.hpp"
 #include "emblib/driver/io/stdio_dev.hpp"
+#include "mp/main.hpp"
 
 static const char* WINDOWS_IP = "172.21.48.1";
 static const char* WSL_IP = "127.0.0.1";
@@ -8,12 +9,15 @@ static const char* WSL_IP = "127.0.0.1";
 int main()
 {
     static emblib::stdio_dev stdio;
-    static mpsim::socket_client socket(WINDOWS_IP, 5000);
     static emblib::mutex socket_mutex;
+
+    static mpsim::socket_client socket(WINDOWS_IP, 5000);
     static mpsim::accelerometer_pb accel(socket, socket_mutex);
+    static mpsim::gyroscope_pb gyro(socket, socket_mutex);
 
     mp::devices_s device_drivers {
         .accelerometer = accel,
+        .gyroscope = gyro,
         .log_device = &stdio
     };
 
